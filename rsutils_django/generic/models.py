cfd5ed33098201b5cdb5833ab350fd373c9db127
dhilipsiva <dhilipsiva@pm.me>
2019-03-05 13:20:07 +0530

    Add generic address

--- rsutils_django/generic/models.py
+++ rsutils_django/generic/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.db.models import (
+    UUIDField, ForeignKey, GenericForeignKey, ContentType, CASCADE)
 
-# Create your models here.
+from rsutils_django.abstract.models import RSAddress
+
+
+class RSGenericAddress(RSAddress):
+    object_id = UUIDField()
+    content_type = ForeignKey(ContentType, on_delete=CASCADE)
+    content_object = GenericForeignKey('content_type', 'object_id')
